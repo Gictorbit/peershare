e28ecf0f3be571b87f394bd95c36c933a1e79bfb
viktor <gictorbit@gmail.com>
2023-04-02 01:13:38 +0330

    fix code generator

--- server/send_file.go
+++ server/send_file.go
@@ -2,24 +2,38 @@ package server
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 	pb "github.com/gictorbit/peershare/api/gen/proto"
-	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (pss *PeerShareService) SendFile(ctx context.Context, req *pb.SendFileRequest) (*pb.SendFileResponse, error) {
-	randomUUID, err := uuid.NewRandom()
+	code, err := pss.GenerateCode()
 	if err != nil {
-		pss.logger.Error("random uuid failed", zap.Error(err))
+		pss.logger.Error("code generator failed", zap.Error(err))
 		return nil, status.Error(codes.Internal, ErrInternalError.Error())
 	}
-	uniqueCode := randomUUID.String()[:10]
+
 	pss.mu.Lock()
 	defer pss.mu.Unlock()
-	pss.sessions[uniqueCode] = req.Offer
+	pss.sessions[code] = req.Offer
 	return &pb.SendFileResponse{
-		Code: uniqueCode,
+		Code: code,
 	}, nil
+}
+
+func (pss *PeerShareService) GenerateCode() (string, error) {
+	// Generate a random 15-byte string
+	randomBytes := make([]byte, 15)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return "", err
+	}
+	// Encode the random bytes as a base64 string
+	code := base64.URLEncoding.EncodeToString(randomBytes)
+	// Truncate the code to 10 characters
+	return code, nil
 }
