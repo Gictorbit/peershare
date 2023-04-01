b5484b9dceeb4266e8924342a38f33eaed37489e
viktor <gictorbit@gmail.com>
2023-04-02 01:17:07 +0330

    interface{} to any

--- client/signal.go
+++ client/signal.go
@@ -91,7 +91,7 @@ func MustReadStdin() string {
 
 // Encode encodes the input in base64
 // It can optionally zip the input before encoding
-func Encode(obj interface{}) string {
+func Encode(obj any) string {
 	b, err := json.Marshal(obj)
 	if err != nil {
 		panic(err)
@@ -106,7 +106,7 @@ func Encode(obj interface{}) string {
 
 // Decode decodes the input from base64
 // It can optionally unzip the input after decoding
-func Decode(in string, obj interface{}) {
+func Decode(in string, obj any) {
 	b, err := base64.StdEncoding.DecodeString(in)
 	if err != nil {
 		panic(err)
